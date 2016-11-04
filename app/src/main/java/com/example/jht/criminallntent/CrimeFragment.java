ff504767c2baa4d821267d40a19009202b3bd6f9
bashibata <qhdxta@gmail.com>
2016-11-04 22:26:35 +0900

    this editor override super class's onCreateView

--- app/src/main/java/com/example/jht/criminallntent/CrimeFragment.java
+++ app/src/main/java/com/example/jht/criminallntent/CrimeFragment.java
@@ -2,6 +2,9 @@ package com.example.jht.criminallntent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by jht on 2016/11/4.
@@ -14,5 +17,11 @@ public class CrimeFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.fragment_crime,container,false);
+        return v;
     }
 }
