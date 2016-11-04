402e8b3e7596265e878ba66fc58b28dc46ec6c28
bashibata <qhdxta@gmail.com>
2016-11-04 22:44:30 +0900

    this editor override super class's onCreateView

--- app/src/main/java/com/example/jht/criminallntent/CrimeFragment.java
+++ app/src/main/java/com/example/jht/criminallntent/CrimeFragment.java
@@ -2,9 +2,12 @@ package com.example.jht.criminallntent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * Created by jht on 2016/11/4.
@@ -12,7 +15,7 @@ import android.view.ViewGroup;
 
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
-
+    private EditText mTitleField;
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -22,6 +25,26 @@ public class CrimeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.fragment_crime,container,false);
+
+        mTitleField = (EditText)v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setmTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
         return v;
     }
+
+
 }
