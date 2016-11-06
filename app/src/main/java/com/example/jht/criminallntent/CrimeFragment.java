2fa09f6848d7679e224875719d4584b8b65daebe
bashibata <qhdxta@gmail.com>
2016-11-07 00:36:50 +0900

    this editor override super class's onCreateView

--- app/src/main/java/com/example/jht/criminallntent/CrimeFragment.java
+++ app/src/main/java/com/example/jht/criminallntent/CrimeFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 /**
@@ -48,6 +49,19 @@ public class CrimeFragment extends Fragment {
 
             }
         });
+
+        mDateButton = (Button)v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getmDate().toString());
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setmSolved(isChecked);
+            }
+        });
+
         return v;
     }
 
